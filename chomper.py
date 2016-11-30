8a15d94a3a0b8596a7127af09d09141b5718e044
hambuol <oliver.hamburger@ssfs.org>
2016-11-30 14:38:10 -0500

    make it so sound happens when mouth eats apple

--- chomper.py
+++ chomper.py
@@ -10,22 +10,24 @@ def main():
     mainsurface = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT), 0, 32)
     pygame.display.set_caption("chomper")
     WHITE = (255,255,255)
-    appleGroup = pygame.sprite.Group()
-    def appl():
-        for x in range(10):
-            xpos = random.randint(0,450)
-            ypos = random.randint(0,450)
-            myapple = apple.Apple(mainsurface)
-            myapple.rect.topleft = (xpos, ypos)
-            myapple.add(appleGroup)
-            mainsurface.blit(myapple.image, myapple.rect)
 
-    appl()
+    appleGroup = pygame.sprite.Group()
+    for x in range(10):
+        xpos = random.randint(0,450)
+        ypos = random.randint(0,450)
+        myapple = apple.Apple(mainsurface)
+        myapple.rect.topleft = (xpos, ypos)
+        myapple.add(appleGroup)
+        mainsurface.blit(myapple.image, myapple.rect)
+
+
     mouthGroup = pygame.sprite.Group()
     mymouth = mouth.Mouth(mainsurface)
     mymouth.rect.topleft = (50, 50)
     mymouth.add(mouthGroup)
     mainsurface.blit(mymouth.image, mymouth.rect)
+    hit = pygame.mixer.Sound("chomp.wav")
+
     while True:
         for event in pygame.event.get():
             if event.type == QUIT:
@@ -35,14 +37,25 @@ def main():
         clock.tick(50)
         mainsurface.fill(WHITE)
         pressed = pygame.key.get_pressed()
-        if pressed[pygame.K_w]:
+        if pressed[pygame.K_UP]:
             mymouth.up()
-        if pressed[pygame.K_s]:
+        if pressed[pygame.K_DOWN]:
             mymouth.down()
-        if pressed[pygame.K_a]:
+        if pressed[pygame.K_LEFT]:
             mymouth.left()
-        if pressed[pygame.K_d]:
+        if pressed[pygame.K_RIGHT]:
             mymouth.right()
         mainsurface.blit(mymouth.image, mymouth.rect)
+        for myapple in appleGroup:
+            mainsurface.blit(myapple.image, myapple.rect)
+
+        mymouth.collide(appleGroup)
+
+
+
+
+
+
+
         pygame.display.update()
 main()
